054e60e2cacabde1a1789a45c39b3d0545106315
Nightlyside <nightlyside@gmail.com>
2021-03-27 15:44:30 +0100

    Triggering github action

--- test/lexer.test.ts
+++ test/lexer.test.ts
@@ -5,28 +5,32 @@ import { lex, simpleCLexer } from "../src/lexer";
 import { Word } from "../src/types";
 
 describe("Lexer functions", () => {
-    it("lex should return no tokens", () => {
-        expect(lex("", words).length).to.be.equal(0);
-    });
+	describe("lex", () => {
+		it("should return no tokens", () => {
+			expect(lex("", words).length).to.be.equal(0);
+		});
 
-    it("lex should return one token", () => {
-        expect(lex("if", words).length).to.be.equal(1);
-    });
+		it("should return one token", () => {
+			expect(lex("if", words).length).to.be.equal(1);
+		});
 
-    it("lex should return several tokens", () => {
-        const tks = lex("def f(a, b):", words);
-        expect(tks.length).to.be.greaterThan(0);
-    });
+		it("should return several tokens", () => {
+			const tks = lex("def f(a, b):", words);
+			expect(tks.length).to.be.greaterThan(0);
+		});
 
-    it("lex should not find a token and return an error", () => {
-        expect(lex("hello", [new Word("test", /test/)])).to.be.empty;
-    });
+		it("should not find a token and return an error", () => {
+			expect(lex("hello", [new Word("test", /test/)])).to.be.empty;
+		});
+	});
 
-    it("simpleCLexer should print tokens", () => {
-        expect(simpleCLexer("void main() {}")).to.be.undefined;
-    });
+	describe("simpleCLexer", () => {
+		it("should print tokens", () => {
+			expect(simpleCLexer("void main() {}")).to.be.undefined;
+		});
 
-    it("simpleCLexer shouldnt find token", () => {
-        expect(simpleCLexer("def f(a, b):")).to.be.undefined;
-    });
+		it("shouldnt find token", () => {
+			expect(simpleCLexer("def f(a, b):")).to.be.undefined;
+		});
+	});
 });
