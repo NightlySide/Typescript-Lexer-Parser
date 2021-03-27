67d00dd38da6d5ac8e31997e9de56d0180fe4a2f
Nightlyside <nightlyside@gmail.com>
2021-03-27 15:51:17 +0100

    Triggering fix

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { printTokens } from "./utils";
 /** The content to analyse, taken from a python test file */
 const src = fs.readFileSync("src/test.py", "utf8");
 
+/** Printing to console */
 console.log("Testing: \n" + src);
 console.log("-------------------");
 const tokens = lex(src, words);
