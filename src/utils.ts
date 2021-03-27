551d2d32eb507f28a09a53a6ebc13aa77b4ae881
Nightlyside <nightlyside@gmail.com>
2021-03-27 15:53:50 +0100

    Fix and trigger

--- src/utils.ts
+++ src/utils.ts
@@ -2,17 +2,17 @@ import { Token } from "./types";
 
 /**
  * Takes a list of tokens and then pretty print them using
- * a table and their name.
+ * a table in the console and their name.
  *
  * @param tokens - the tokens to pretty print
  */
 function printTokens(tokens: Token[]): void {
-    const res: string[][] = [];
+	const res: string[][] = [];
 
-    // for each token extract the name and it's value
-    tokens.map((t: Token) => res.push([t.word.name, t.value]));
-    // then print it to the console using the table function
-    console.table(res);
+	// for each token extract the name and it's value
+	tokens.map((t: Token) => res.push([t.word.name, t.value]));
+	// then print it to the console using the table function
+	console.table(res);
 }
 
 export { printTokens };
